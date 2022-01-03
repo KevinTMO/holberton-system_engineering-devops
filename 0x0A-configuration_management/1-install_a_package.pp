9bfc8c7e92b739343352fb97d4b1303ce16607f6
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-03 19:56:26 +0000

    Undoing last step and using gen ver

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Resource Type: package -> installing puppet-lint Trying: global var
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
+$style = 'puppet-lint'
+
+package { $style:
+  ensure   => '1.1.0',
   provider => 'gem'
 }
