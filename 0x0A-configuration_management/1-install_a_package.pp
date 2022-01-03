4ddc41c3fe6cc0d4dc1cbf06cb7cc7fb21735843
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-03 19:51:14 +0000

    Removing global var to validate check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # Resource Type: package -> installing puppet-lint Trying: global var
 
-$style = 'puppet-lint'
-
-package { $style:
+package { 'puppet-lint':
   ensure   => '2.5.0',
   provider => 'gem'
 }
