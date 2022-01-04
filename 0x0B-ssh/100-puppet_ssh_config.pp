5f27a76e8f4356178a00959b3df217da5baa1c8d
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-04 17:43:47 +0000

    Adding lines to config using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Adding lines to ssh_config using puppet
+
+file_line { 'sudo_rule_1':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'BatchMode yes'
+}
+
+file_line { 'sudo_rule_2':
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentifyFile ~/.ssh/school'
+}
